ba12dd24e5c9a34fee3bd74fd500c1cb6a6f5a93
Subhashis Ghosh <iamsubhashis15@gmail.com>
2026-08-13 22:39:49 +0530

    Rename App.jsx to MainApp.jsx to bypass git case cache

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react'
-import App from "./App";
+import ReactDOM from 'react-dom/client'
+import MainApp from './MainApp' // Changed from ./App to ./MainApp
 import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MainApp />
+  </React.StrictMode>,
+)
 
 // ==================================================
 // STOREERP API AUTHENTICATION
