bbe1a2f47c108da85356cb8669a1bc019e9130b4
Subhashis Ghosh <iamsubhashis15@gmail.com>
2026-08-13 22:49:12 +0530

    Fix main.jsx to import MainApp instead of App

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // 👈 Notice NO .jsx extension here!
+import MainApp from './MainApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
 )
 
