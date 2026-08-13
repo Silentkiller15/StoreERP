f7c0c6ed2704530d620d87f88df7d17b0230e4f5
Subhashis Ghosh <iamsubhashis15@gmail.com>
2026-08-13 21:51:17 +0530

    Fix script paths and main.jsx imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import axios from "axios";
-
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react'
+import App from './App.jsx'
+import './index.css'
 
 // ==================================================
 // STOREERP API AUTHENTICATION
